174f7be5e4e7febd49f03b0be3e58be1177cd9e8
songruirui <willsongrui@gmail.com>
2013-08-08 09:16:51 +0800

    fix a bug about @cart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '68ea455e63e5377b0e5247126b79d0f02d5851e0ab10534ce5ef24f8da9663961d6808ac6c9a4a607fcc70b7b003a99d4863b88890edcd5cfd44a153b44261c5'
